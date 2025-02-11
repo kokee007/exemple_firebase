81e5e44c87a12f7fa7861a66b70d256b184bab38
kokee007 <alexalvarez@ceroca.cat>
2025-02-11 17:08:16 +0100

    Crear textfield i boto auth dins del main

---
+++ lib/pagines/pagina_registre.dart
@@ -0,0 +1,124 @@
+import 'package:exemple_firebase/Components/boto_auth.dart';
+import 'package:exemple_firebase/Components/textfield_auth.dart';
+import 'package:flutter/material.dart';
+
+class PaginaRegistre extends StatelessWidget {
+  const PaginaRegistre({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final TextEditingController tecEmail = TextEditingController();
+    final TextEditingController tecPassword = TextEditingController();
+    final TextEditingController tecConfPass = TextEditingController();
+
+
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 2500, 183, 159),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //Logo
+                Icon(
+                  Icons.fireplace,
+                  size: 120,
+                  color: Color.fromARGB(255, 255, 240, 218),
+                ),
+          
+                SizedBox(height: 25),
+          
+                //Frase
+                Text(
+                  "Crea un compte nou",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 255, 240, 218),
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+          
+                SizedBox(height: 25),
+          
+                //Text divisori
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          thickness: 1,
+                          color: Color.fromARGB(255, 255, 240, 218),
+                        ),
+                      ),
+                  
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 4),
+                        child: Text(
+                          "Registra't",
+                          style: TextStyle(color: Color.fromARGB(255, 355, 240, 218)),
+                          ),
+                      ),
+                  
+                        Expanded(
+                        child: Divider(
+                          thickness: 1,
+                          color: Color.fromARGB(255, 255, 240, 218),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+          
+                //TextField Email
+                TextfieldAuth(controller: tecEmail,
+                obscureText: false,
+                hintText: "Escriu el teu email...",
+                ),
+          
+                //TextField Password
+                TextfieldAuth(controller: tecPassword,
+                obscureText: true,
+                hintText: "Escriu la teva contrasenya...",
+                ),
+          
+                //TextField Confirmar password
+                TextfieldAuth(controller: tecConfPass,
+                obscureText: true,
+                hintText: "Reescriu la contrasenya...",
+                ),
+          
+                SizedBox(height: 10,),
+          
+          
+                //no estas registrat?
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Ja ets membre?"),
+                    SizedBox(width: 5,),
+                    GestureDetector(
+                      child: Text(
+                        "Fes login",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 40, 71, 97),
+                        ),
+                        ),
+                    ),
+                  ],
+                ),
+
+                SizedBox( height:10 ,),
+                
+                //Botó registra't
+                BotoAuth(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
