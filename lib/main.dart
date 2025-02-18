83b9b2d66902b9fd6ca4e047d149323a68b74db0
kokee007 <alexalvarez@ceroca.cat>
2025-02-18 16:25:57 +0100

    Firebase apunt

--- lib/main.dart
+++ lib/main.dart
@@ -19,3 +19,34 @@ class MainApp extends StatelessWidget {
       );
   }
 }
+
+/*
+1) Tenir node .js instalat
+  - En acabar es pot comprobar fent:
+  node -v
+  npm -v
+
+  2) Anar a la web de firebase i clicar a "go to console"
+    -Tenir en compte el compte de google, "alexalvarez@ceroca.cat"
+
+3)Desde la consola de Firebase, creem un projecte.
+
+4)Anem al menú Compilación i habilitem: 
+  Authentication i FireStore Database.
+
+5) En una cmd, per exemple la del VS code, fem:
+npm install -g firebase-tools
+(aixo instala Firebase al dispositiu)
+
+6)en terminal del visual
+- Flutter pub global activate flutterfire_cli
+- dart pub global activate flutterfire_cli
+
+7)-flutter configure ´FALTA!´
+-Seleccionem el projecte de firebase
+amb el que volem vincular
+
+
+
+export PATH= "$HOME/ .pub-cache/bin
+*/
